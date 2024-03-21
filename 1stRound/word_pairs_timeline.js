1dbbcb49037f21df36a27fe068a12fb7e44e6408
Butavand <55365606+Butavand@users.noreply.github.com>
2024-03-21 16:55:40 +0100

    Add files via upload

---
+++ 1stRound/word_pairs_timeline.js
@@ -0,0 +1,32 @@
+// Build TIMELINE
+
+var timeline = [];
+
+timeline.push(whichParticipant, whichVersion);
+
+timeline.push(
+    instructionsPractice,
+    practiceRunEncodingA, practiceRunEncodingB,
+    practiceRunTestA, practiceRunTestB,
+);
+
+        timeline.push(instructionsFirstTrial);
+        timeline.push(
+            runEncodingA, runEncodingB,
+            instructionsBreak
+        )
+        
+        timeline.push(
+            pvtStartScreen1,
+            pvtStartScreen2, 
+            pvtPracticeLoop, 
+            pvtMainInstructions, 
+            pvtMainProcedure, 
+            ifMissingFeedback,
+            pvtEnd
+        );
+     
+    timeline.push(
+        instructionsTest,
+        runTestA, runTestB
+    );  
---
+++ 1stRound/wordsPractice.js
@@ -0,0 +1,31 @@
+var practiceStimuli = [
+      [
+        {
+          "cuePractice": "INSTANZ",
+          "targetPractice": "GESETZGEBUNG"
+        },
+        {
+          "cuePractice": "GEDULD",
+          "targetPractice": "KONTROLLE"
+        },
+        {
+          "cuePractice": "GITTER",
+          "targetPractice": "BESCHRÄNKUNG"
+        }
+      ],
+      [
+        {
+          "cuePractice": "TÜR",
+          "targetPractice": "SCHRANK"
+        },
+        {
+          "cuePractice": "KAMERAD",
+          "targetPractice": "OPFER"
+        },
+        {
+          "cuePractice": "BÜRGSCHAFT",
+          "targetPractice": "VOLLMACHT"
+        }
+      ]
+
+];
