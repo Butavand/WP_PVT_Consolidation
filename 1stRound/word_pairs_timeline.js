fffa9c37cadbe04534ea6b038f7764dd03591078
einGlasRotwein <julivnagel@gmail.com>
2024-04-25 21:19:56 +0200

    clean up timeline

--- 1stRound/word_pairs_timeline.js
+++ 1stRound/word_pairs_timeline.js
@@ -2,31 +2,26 @@
 
 var timeline = [];
 
-timeline.push(whichParticipant, whichVersion);
-
 timeline.push(
+    whichParticipant, whichVersion,
+
     instructionsPractice,
     practiceRunEncodingA, practiceRunEncodingB,
     practiceRunTestA, practiceRunTestB,
-);
 
-        timeline.push(instructionsFirstTrial);
-        timeline.push(
-            runEncodingA, runEncodingB,
-            instructionsBreak
-        )
-        
-        timeline.push(
-            pvtStartScreen1,
-            pvtStartScreen2, 
-            pvtPracticeLoop, 
-            pvtMainInstructions, 
-            pvtMainProcedure, 
-            ifMissingFeedback,
-            pvtEnd
-        );
-     
-    timeline.push(
-        instructionsTest,
-        runTestA, runTestB
-    );  
+    instructionsFirstTrial,
+
+    runEncodingA, runEncodingB,
+    instructionsBreak,
+
+    pvtStartScreen1,
+    pvtStartScreen2, 
+    pvtPracticeLoop, 
+    pvtMainInstructions, 
+    pvtMainProcedure, 
+    ifMissingFeedback,
+    pvtEnd,
+
+    instructionsTest,
+    runTestA, runTestB, instructionsThanksMessage
+);
